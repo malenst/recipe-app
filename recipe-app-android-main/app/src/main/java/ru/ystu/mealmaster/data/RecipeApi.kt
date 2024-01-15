34458fc7285bf13de14946d89a7af4988f1759d8
Ilya Kuzmenko <ikuzmenko@solit-clouds.ru>
2024-01-15 23:40:41 +0300

    package deletion

--- recipe-app-android-main/app/src/main/java/ru/ystu/mealmaster/data/RecipeApi.kt
+++ recipe-app-android-main/app/src/main/java/ru/ystu/mealmaster/data/RecipeApi.kt
@@ -1,35 +1,38 @@
 package ru.ystu.mealmaster.data
 
-import ru.ystu.mealmaster.domain.*
 import retrofit2.Call
 import retrofit2.http.*
-import java.util.UUID
+import ru.ystu.mealmaster.domain.Category
+import ru.ystu.mealmaster.domain.Recipe
+import ru.ystu.mealmaster.domain.Review
+import ru.ystu.mealmaster.domain.ReviewData
+import java.util.*
 
 interface RecipeApi {
 
     @GET("recipe")
-    fun getRecipes() : Call<ru.ystu.mealmaster.data.ApiResponseDto<List<Recipe>>>
+    fun getRecipes() : Call<ApiResponseDto<List<Recipe>>>
 
     @GET("recipe/popular")
-    fun getTop10Recipes() : Call<ru.ystu.mealmaster.data.ApiResponseDto<List<Recipe>>>
+    fun getTop10Recipes() : Call<ApiResponseDto<List<Recipe>>>
 
     @GET("recipe/{id}")
-    fun getRecipeById(@Path("id") id : UUID) : Call<ru.ystu.mealmaster.data.ApiResponseDto<Recipe>>
+    fun getRecipeById(@Path("id") id : UUID) : Call<ApiResponseDto<Recipe>>
 
     @GET("recipe")
-    fun getRecipesByCategory(@Query("category") category : String) : Call<ru.ystu.mealmaster.data.ApiResponseDto<List<Recipe>>>
+    fun getRecipesByCategory(@Query("category") category : String) : Call<ApiResponseDto<List<Recipe>>>
 
     @GET("category")
-    fun getCategories() : Call<ru.ystu.mealmaster.data.ApiResponseDto<List<Category>>>
+    fun getCategories() : Call<ApiResponseDto<List<Category>>>
 
     @Headers("Content-Type: application/json", "Accept: application/json")
     @POST("add-review/recipe/{id}")
-    fun addReview(@Path("id") id: UUID, @Body body: ReviewData) : Call<ru.ystu.mealmaster.data.ApiResponseDto<Review>>
+    fun addReview(@Path("id") id: UUID, @Body body: ReviewData) : Call<ApiResponseDto<Review>>
 
     @Headers("Content-Type: application/json", "Accept: application/json")
     @POST("login")
     fun login(
         @Query("username") username : String,
         @Query("password") password : String
-    ) : Call<ru.ystu.mealmaster.data.ApiResponseDto<List<Recipe>>>
+    ) : Call<ApiResponseDto<List<Recipe>>>
 }
